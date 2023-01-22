86659f962ae7f986bddf4fc86bd10fff2bafc71b
cyrusnaficy <cyrus.naficy@gmail.com>
2023-01-22 02:16:05 -0500

    added claw to subsystem init and command boilerplate

---
+++ command/claw.py
@@ -0,0 +1,15 @@
+from robotpy_toolkit_7407.command import SubsystemCommand
+
+from subsystem import Claw
+
+class ClawCommand(SubsystemCommand[Claw]):
+
+    def __init__(self, subsystem: Claw):
+        super().__init__(subsystem)
+        self.subsystem = subsystem
+
+    def initialize(self) -> None:
+        pass
+
+    def execute(self) -> None:
+        pass
