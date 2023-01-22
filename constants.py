9fe36dfcf7f8fd541f96f911c0ada30e790a25e1
cyrusnaficy <cyrus.naficy@gmail.com>
2023-01-22 02:23:02 -0500

    fix conflicting file

--- constants.py
+++ constants.py
@@ -1,3 +1,33 @@
+import math
+
+from robotpy_toolkit_7407.motors.rev_motors import (
+    rev_sensor_accel_unit,
+    rev_sensor_unit,
+    rev_sensor_vel_unit
+)
+
+from robotpy_toolkit_7407.utils.units import deg, ft, hour, inch, m, mile, rad, rev, s
+
+period = 0.03
+
+# --- DRIVETRAIN ---
+# drivetrain_turn_gear_ratio = ((8.16 * 4096)/(2*math.pi) * rev_sensor_unit / rad).asNumber()
+drivetrain_turn_gear_ratio = 80.4848
+drivetrain_move_gear_ratio = ((544318 * rev_sensor_unit) / (511 * inch)).asNumber(
+    rad / m
+)
+
+track_width = (24.2 * inch).asNumber(m)
+
+# TODO Maybe change these
+drivetrain_max_vel = (20 * mile / hour).asNumber(m / s)
+drivetrain_target_max_vel = (7 * mile / hour).asNumber(m / s)
+drivetrain_max_angular_vel = (2 * rev / s).asNumber(rad / s)
+drivetrain_max_climb_vel = (2 * mile / hour).asNumber(m / s)
+
+
+# --- CLAW ---
+
 drivetrain_turn_gear_ratio = 80.4848
 
 # Dummy data
