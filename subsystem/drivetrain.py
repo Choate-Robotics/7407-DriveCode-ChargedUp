5544fe117091fe2d7f0715f390c49748cf8a0e5e
Sidharth Rao <sidharthmrao@gmail.com>
2023-01-23 22:31:18 -0500

    Added some custom units.

--- subsystem/drivetrain.py
+++ subsystem/drivetrain.py
@@ -7,16 +7,17 @@ import wpilib
 from robotpy_toolkit_7407.motors.rev_motors import SparkMax, SparkMaxConfig
 from robotpy_toolkit_7407.subsystem_templates.drivetrain import SwerveNode, SwerveDrivetrain, SwerveGyro
 from oi.keymap import Keymap
+from units.SI import *
 
-from robotpy_toolkit_7407.utils.units import (
-    deg,
-    meters,
-    meters_per_second,
-    rad,
-    radians,
-    radians_per_second,
-    s,
-)
+# from robotpy_toolkit_7407.utils.units import (
+#     deg,
+#     meters,
+#     meters_per_second,
+#     rad,
+#     radians,
+#     radians_per_second,
+#     s,
+# )
 
 TURN_CONFIG = SparkMaxConfig(
     0.2, 0, 0.003, 0.00015, (-0.5, 0.5), rev.CANSparkMax.IdleMode.kBrake
@@ -25,52 +26,66 @@ MOVE_CONFIG = SparkMaxConfig(
     0.00005, 0, 0.0004, 0.00017, idle_mode=rev.CANSparkMax.IdleMode.kBrake
 )
 
+
 class SparkMaxSwerveNode(SwerveNode):
-    def __init__(self, m_move: SparkMax, m_turn: SparkMax, encoder: wpilib.AnalogEncoder, encoder_zeroed_absolute_pos: float = 0, drive_reversed: bool = False, turn_reversed: bool = False):
+    def __init__(self, m_move: SparkMax, m_turn: SparkMax, encoder: wpilib.AnalogEncoder,
+                 encoder_zeroed_absolute_pos: float = 0, drive_reversed: bool = False, turn_reversed: bool = False):
         self.m_move = m_move
         self.m_turn = m_turn
         self.encoder = encoder
         self.encoder_zeroed_absolute_pos = encoder_zeroed_absolute_pos
         self.drive_reversed = drive_reversed
         self.turn_reversed = turn_reversed
-        self.m_move.init()
-        self.m_turn.init()
 
     def init(self):
         super().init()
-        
+        self.m_move.init()
+        self.m_turn.init()
+
     # make the turn motor set their sensor 0 to current horizontal thingy
     def zero(self):
         current_pos_rad = self.encoder.getAbsolutePosition()
         zeroed_pos_rad = self.encoder_zeroed_absolute_pos
         new_pos_rad = current_pos_rad - zeroed_pos_rad
 
-        self.m_turn.set_sensor_position(new_pos_rad * constants.drivetrain_turn_gear_ratio / (2 * math.pi))
-    
+        new_pos_rotations: rotation_motor_encoder_units = new_pos_rad * radians_to_rotations * swerve_pod_rotations__to__rotation_motor_encoder_units
+
+        self.m_turn.set_sensor_position(new_pos_rotations)
+
     # reposition the wheels
     def set_motor_angle(self, pos: radians):
         if self.turn_reversed:
             pos *= -1
+
+        target_position: rotation_motor_encoder_units = pos * radians_to_rotations * swerve_pod_rotations__to__rotation_motor_encoder_units
+
         self.m_turn.set_target_position(
-            (pos / (2 * math.pi)) * constants.drivetrain_turn_gear_ratio
+            target_position
         )
 
     def direct_set_motor_angle(self, pos: radians):
+        target_position: rotation_motor_encoder_units = pos * radians_to_rotations * swerve_pod_rotations__to__rotation_motor_encoder_units
+
         self.m_turn.set_target_position(
-            (pos / (2 * math.pi)) * constants.drivetrain_turn_gear_ratio
+            target_position
         )
 
     def get_current_motor_angle(self) -> radians:
-        return self.m_turn.get_sensor_position() / constants.drivetrain_turn_gear_ratio
+        current_motor_angle_radians: radians = self.m_turn.get_sensor_position() * rotation_motor_encoder_units__to__swerve_pod_rotations * rotations_to_radians
+        return current_motor_angle_radians
 
     # rotate the wheel so the robot moves
     def set_motor_velocity(self, vel: meters_per_second):
         if self.drive_reversed:
             vel *= -1
-        self.m_move.set_target_velocity(vel * constants.drivetrain_move_gear_ratio)
+
+        target_velocity: drive_motor_encoder_units = vel * meters__to__drive_motor_encoder_units
+        self.m_move.set_target_velocity(target_velocity)
 
     def get_motor_velocity(self) -> radians_per_second:
-        return self.m_move.get_sensor_velocity() / constants.drivetrain_move_gear_ratio
+        sensor_velocity: radians_per_second = self.m_move.get_sensor_velocity() * rotation_motor_encoder_units__to__swerve_pod_rotations * rotations_to_radians
+        return sensor_velocity
+
 
 class Drivetrain(SwerveDrivetrain):
     n_00 = SparkMaxSwerveNode(
@@ -107,7 +122,7 @@ class Drivetrain(SwerveDrivetrain):
         encoder_zeroed_absolute_pos=0.990,
         turn_reversed=True,
         drive_reversed=True,
-        
+
     )
 
     axis_dx = Keymap.Drivetrain.DRIVE_X_AXIS
@@ -117,7 +132,4 @@ class Drivetrain(SwerveDrivetrain):
     max_vel: meters_per_second = constants.drivetrain_max_vel
     max_angular_vel: radians_per_second = constants.drivetrain_max_angular_vel
     deadzone_velocity: meters_per_second = 0.01
-    deadzone_angular_velocity: radians_per_second = (5 * deg / s).asNumber(rad / s)
-
-
-
+    deadzone_angular_velocity: radians_per_second = 5 * degrees_per_second__to__radians_per_second
---
+++ units/SI.py
@@ -0,0 +1,98 @@
+"""
+SI units for use in typing and conversion.
+"""
+
+# Distance
+import math
+import config
+import constants
+
+meters = float
+meters_to_inches = 39.3701
+meters_to_feet = meters_to_inches / 12
+meters_to_yards = meters_to_feet / 3
+meters_to_miles = meters_to_yards / 1760
+
+inches = float
+inches_to_meters = 1 / meters_to_inches
+inches_to_feet = inches_to_meters * meters_to_feet
+inches_to_yards = inches_to_meters * meters_to_yards
+inches_to_miles = inches_to_meters * meters_to_miles
+
+feet = float
+feet_to_meters = 1 / meters_to_feet
+feet_to_inches = feet_to_meters * meters_to_inches
+feet_to_yards = feet_to_meters * meters_to_yards
+feet_to_miles = feet_to_meters * meters_to_miles
+
+yards = float
+yards_to_meters = 1 / meters_to_yards
+yards_to_inches = yards_to_meters * meters_to_inches
+yards_to_feet = yards_to_meters * meters_to_feet
+yards_to_miles = yards_to_meters * meters_to_miles
+
+miles = float
+miles_to_meters = 1 / meters_to_miles
+miles_to_inches = miles_to_meters * meters_to_inches
+miles_to_feet = miles_to_meters * meters_to_feet
+miles_to_yards = miles_to_meters * meters_to_yards
+
+# Rotation
+rotations = float
+rotations_to_degrees = 360
+rotations_to_radians = 2 * math.pi
+
+degrees = float
+degrees_to_rotations = 1 / rotations_to_degrees
+degrees_to_radians = degrees_to_rotations * rotations_to_radians
+
+radians = float
+radians_to_rotations = 1 / rotations_to_radians
+radians_to_degrees = radians_to_rotations * rotations_to_degrees
+
+# Time
+seconds = float
+seconds_to_minutes = 1 / 60
+seconds_to_hours = seconds_to_minutes / 60
+seconds_to_days = seconds_to_hours / 24
+
+minutes = float
+minutes_to_seconds = 1 / seconds_to_minutes
+minutes_to_hours = minutes_to_seconds * seconds_to_hours
+minutes_to_days = minutes_to_seconds * seconds_to_days
+
+hours = float
+hours_to_seconds = 1 / seconds_to_hours
+hours_to_minutes = hours_to_seconds * seconds_to_minutes
+hours_to_days = hours_to_seconds * seconds_to_days
+
+days = float
+days_to_seconds = 1 / seconds_to_days
+days_to_minutes = days_to_seconds * seconds_to_minutes
+days_to_hours = days_to_seconds * seconds_to_hours
+
+# Velocity
+meters_per_second = float
+
+radians_per_second = float
+radians_per_second__to__degrees_per_second = radians_to_degrees
+radians_per_second__to__rotations_per_second = radians_to_rotations
+
+degrees_per_second = float
+degrees_per_second__to__radians_per_second = degrees_to_radians
+degrees_per_second__to__rotations_per_second = degrees_to_rotations
+
+rotations_per_second = float
+rotations_per_second__to__radians_per_second = rotations_to_radians
+rotations_per_second__to__degrees_per_second = rotations_to_degrees
+
+rotation_motor_encoder_units = float
+rotation_motor_encoder_units__to__swerve_pod_rotations = 1 / constants.drivetrain_turn_gear_ratio
+
+swerve_pod_rotations = float
+swerve_pod_rotations__to__rotation_motor_encoder_units = 1 / rotation_motor_encoder_units__to__swerve_pod_rotations
+
+drive_motor_encoder_units = float
+drive_motor_encoder_units__to__meters = 1 / constants.drivetrain_move_gear_ratio
+
+meters__to__drive_motor_encoder_units = 1 / drive_motor_encoder_units__to__meters
