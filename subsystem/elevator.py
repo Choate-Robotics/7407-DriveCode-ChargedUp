9bf05fb7f9c62b898ff5c809722683de2c40ca2d
Inspirol <91295122+Inspirol@users.noreply.github.com>
2023-01-26 02:31:36 -0500

    adds limit switch and abs encoder

--- subsystem/elevator.py
+++ subsystem/elevator.py
@@ -22,20 +22,29 @@ class Elevator(Subsystem):  # elevator class
     brake: wpilib.Solenoid = wpilib.Solenoid(
         1, wpilib.PneumaticsModuleType.REVPH, config.elevator_brake_id)  # brake that holds the arm in place
     initialized: bool = False
-    extend_sensor = LimitSwitch(9)  # senses when extension is at zero
-    turn_sensor = rev.CANSparkMax(config.elevator_left_rotation_motor_id, rev.MotorType.kBrushless).getAbsoluteEncoder(
-        rev._rev.SparkMaxAbsoluteEncoder.Type.kDutyCycle)  # senses the rotation of the elevator
+    elevator_bottom_sensor = rev.CANSparkMax(config.elevator_left_rotation_motor_id, rev.MotorType.kBrushless).getReverseLimitSwitch(rev.SparkMaxLimitSwitch.Type.kNormallyOpen)
+    elevator_top_sensor = rev.CANSparkMax(config.elevator_left_rotation_motor_id, rev.MotorType.kBrushless).getForwardLimitSwitch(rev.SparkMaxLimitSwitch.Type.kNormallyOpen)
+    turn_sensor = rev.CANSparkMax(config.elevator_left_rotation_motor_id, rev.MotorType.kBrushless).getAnalog()  # senses the rotation of the elevator
     brake_enabled: bool = False
     pose = Pose3d(0, 0, constants.pivot_point_height, 0, 0, 0)
     elevator_length: float
+    rotation_overide: bool = False # if true, the rotation limits will be ignored
+    extension_overide: bool = False # if true, the extension limits will be ignored
+    brake_overide: bool = False # if true, the brake will not be enabled or disabled
 
     def enable_brake(self):
-        self.brake_enabled = True
-        self.brake.set(True)
+        if self.brake_overide:
+            return
+        else:
+            self.brake_enabled = True
+            self.brake.set(True)
 
     def disable_brake(self):
-        self.brake_enabled = False
-        self.brake.set(False)
+        if self.brake_overide:
+            return
+        else:
+            self.brake_enabled = False
+            self.brake.set(False)
 
     def get_pose(self):
         # returns the pose of the arm
@@ -64,46 +73,50 @@ class Elevator(Subsystem):  # elevator class
 
     def boundary_box(self, radians: float):
 
-        degree = abs(radians)
+        if self.extension_overide:
+            return 1
+        else:
 
-        def get_length_ratio(angle, adjacent):
-            # Gets the length of the angle using cosine
-            max_length = adjacent / math.cos(angle)
-            # if the length of the angle is greater than the max_elevator_height, return one
-            # else give a percentage of the height of the current line and the max_elevator_height
-            # protects against any irregular numbers heigher than the max elevator height by always
-            # defaulting to 1
-            if max_length > constants.max_elevator_height:
-                return 1
-            else:
-                return max_length / constants.max_elevator_height  # where is the hype at
+            degree = abs(radians)
 
-        # Finds the length of each the dimensions for the boundry box using the constants
-        top_box_height = constants.vertical_boundary - constants.pivot_point_height
-        bottom_box_height = constants.pivot_point_height
-        horizontal_length = 0.5(constants.robot_length) + \
-            constants.horizontal_boundary
+            def get_length_ratio(angle, adjacent):
+                # Gets the length of the angle using cosine
+                max_length = adjacent / math.cos(angle)
+                # if the length of the angle is greater than the max_elevator_height, return one
+                # else give a percentage of the height of the current line and the max_elevator_height
+                # protects against any irregular numbers heigher than the max elevator height by always
+                # defaulting to 1
+                if max_length > constants.max_elevator_height:
+                    return 1
+                else:
+                    return max_length / constants.max_elevator_height  # where is the hype at
 
-        # finds the angle on each side where the length is equal to the max_elevator_height
-        top_box_angle = math.acos(top_box_height/constants.max_elevator_height)
-        side_box_angle = math.acos(
-            horizontal_length/constants.max_elevator_height)
-        bottom_box_angle = math.acos(
-            bottom_box_height/constants.max_elevator_height)
+            # Finds the length of each the dimensions for the boundry box using the constants
+            top_box_height = constants.vertical_boundary - constants.pivot_point_height
+            bottom_box_height = constants.pivot_point_height
+            horizontal_length = 0.5(constants.robot_length) + \
+                constants.horizontal_boundary
 
-        # if the degree is within the minimun range of each of these boundry sides
+            # finds the angle on each side where the length is equal to the max_elevator_height
+            top_box_angle = math.acos(top_box_height/constants.max_elevator_height)
+            side_box_angle = math.acos(
+                horizontal_length/constants.max_elevator_height)
+            bottom_box_angle = math.acos(
+                bottom_box_height/constants.max_elevator_height)
 
-        # assuming the top is zero...
+            # if the degree is within the minimun range of each of these boundry sides
 
-        # if the degree is within the max_elevator_extension and facing the top
-        if degree < top_box_angle:
-            return get_length_ratio(degree, top_box_angle)
-        # if the degree is within the max_elevator_extension and facing the sides
-        elif (degree) > side_box_angle and degree < side_box_angle+(90 - side_box_angle):
-            return get_length_ratio(90 - degree, side_box_angle)
-        # if the degree is within the max_elevator_extension and facing the bottom
-        elif (degree) > bottom_box_angle:
-            return get_length_ratio(180 - degree, bottom_box_angle)
+            # assuming the top is zero...
+
+            # if the degree is within the max_elevator_extension and facing the top
+            if degree < top_box_angle:
+                return get_length_ratio(degree, top_box_angle)
+            # if the degree is within the max_elevator_extension and facing the sides
+            elif (degree) > side_box_angle and degree < side_box_angle+(90 - side_box_angle):
+                return get_length_ratio(90 - degree, side_box_angle)
+            # if the degree is within the max_elevator_extension and facing the bottom
+            elif (degree) > bottom_box_angle:
+                return get_length_ratio(180 - degree, bottom_box_angle)
 
     def set_length(self, length: meters):  # set arm extension
         if self.boundary_box(length) > self.elevator_length:
@@ -124,7 +137,10 @@ class Elevator(Subsystem):  # elevator class
 
     def soft_limits(self, radians: float):
         # returns if the degree is within the soft limits
-        return radians < math.radians(config.elevator_max_rotation) and radians > math.radians(config.elevator_min_rotation)
+        if self.rotation_overide:
+            return True
+        else:
+            return radians < math.radians(config.elevator_max_rotation) and radians > math.radians(config.elevator_min_rotation)
 
     def set_rotation(self, radians: float):  # set arm rotation
         # if the rotation is within the soft limits, set the rotation to the given degree
