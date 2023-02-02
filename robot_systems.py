7efc4ce7cb164f6b95af4ca0db68b70c9caa20aa
cyrusnaficy <cyrus.naficy@gmail.com>
2023-02-02 14:09:20 -0500

    put sparkmax inside of robot_systems

--- robot_systems.py
+++ robot_systems.py
@@ -1,11 +1,15 @@
-import subsystem
-import sensors
-import wpilib
+import rev
 from subsystem import Drivetrain, Claw
+from robotpy_toolkit_7407.motors.rev_motors import SparkMax, SparkMaxConfig
 
+MOVE_CONFIG = SparkMaxConfig(
+    0.00005, 0, 0.0004, 0.00017, idle_mode=rev.CANSparkMax.IdleMode.kBrake
+)
 class Robot:
     drivetrain = Drivetrain()
-    claw = Claw()
+    claw = Claw({
+        "claw_motor": SparkMax(can_id=1, config=MOVE_CONFIG),
+    })
     pass
 
 class Pneumatics:
