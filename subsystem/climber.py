2936b1e218beedb606df9f79ac0889bdc0774d64
Carson Ling <you@example.com>
2023-02-03 08:34:59 -0500

    deleted wrong stuff in climber subsystem

--- subsystem/climber.py
+++ subsystem/climber.py
@@ -27,16 +27,4 @@ class Climber(Subsystem):
         self.l_piston = DoubleSolenoidPiston(config.l_piston_module, config.l_piston_forwardChannel, config.l_piston_reverseChannel)
         self.r_piston = DoubleSolenoidPiston(config.r_piston_module, config.r_piston_forwardChannel, config.r_piston_reverseChannel)
 
-    def zero(self):
-        current_pos_rad = (
-                math.radians(self.encoder.getAbsolutePosition())
-                - self.absolute_encoder_zeroed_pos
-        )
-
-        self.m_turn.set_sensor_position(
-            current_pos_rad * constants.drivetrain_turn_gear_ratio / (2 * math.pi)
-        )
-        self.set_motor_angle(current_pos_rad)
-
-    def raw_output(self, power):
-        self.m_move.set_raw_output(power)
+    
