b99ad3e6937fbd2c1600c651a283add90c888a84
Sidharth Rao <sidharthmrao@gmail.com>
2023-02-03 23:52:44 -0500

    Redo the refactoring properly.

--- autonomous/__init__.py
+++ autonomous/__init__.py
@@ -1,2 +1,2 @@
-from autonomous.routines.basic_auto.square_auto import routine
+from autonomous.routines.square_auto.square_auto import routine
 from autonomous.utils.trajectory import CustomTrajectory
