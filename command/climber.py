a621dbd9aa6e616aec5d95e4dfd2137eabf0d4e7
Carson Ling <you@example.com>
2023-02-10 08:52:04 -0500

    added gyro to climber

--- command/climber.py
+++ command/climber.py
@@ -5,6 +5,7 @@ import config
 import constants
 from subsystem import Climber
 from robot_systems import Robot, Sensors
+import math
 
 
 class ClimberZero(SubsystemCommand[Climber]):
@@ -60,10 +61,11 @@ class ClimberPivot(SubsystemCommand[Climber]):
         pass
 
     def isFinished(self) -> bool:
-        return Sensors.gyro
+        return Sensors.gyro.get_robot_pitch() >= Robot.climber.pivot_threshold/180*math.pi
     
     def end(self, interrupted=False):
         Robot.climber.climber_motor.set_raw_output(0)
+        Robot.climber.climber_motor.brake.set(True)
 
 
 
