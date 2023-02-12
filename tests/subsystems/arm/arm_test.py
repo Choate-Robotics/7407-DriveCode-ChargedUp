fdc586314982e156dc14c70486163a4e18c92f3a
Sidharth Rao <sidharthmrao@gmail.com>
2023-02-12 12:46:47 -0600

    Update arm_test.py

--- tests/subsystems/arm/arm_test.py
+++ tests/subsystems/arm/arm_test.py
@@ -3,8 +3,3 @@ import pytest
 from wpimath.geometry import Pose2d, Translation2d, Rotation2d
 from subsystem.arm import Elevator
 
-
-from logging import Logger
-
-LOGGING = Logger("ARM_SYSTEMS_TESTS")
-
