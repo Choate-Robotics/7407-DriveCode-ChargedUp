468aee59c815ce50524d371a73246e200b63bb75
Ann Song <gmsong1978@gmail.com>
2023-03-04 17:17:20 -0500

    shifted red_score_right to the left, blue_score_left to the right

--- autonomous/routines/cone_cube_score_left/red_cone_cube_score_right.py
+++ autonomous/routines/cone_cube_score_left/red_cone_cube_score_right.py
@@ -29,7 +29,7 @@ path_1 = FollowPathCustom(
     trajectory=CustomTrajectory(
         start_pose=Pose2d(initial_x, initial_y, math.radians(0)),
         waypoints=[],
-        end_pose=Pose2d(6.45, config.field_width - (1 + 3.66), math.radians(0)),
+        end_pose=Pose2d(6.45, config.field_width - (1 + 3.66 - 0.3), math.radians(0)),
         max_velocity=max_vel,
         max_accel=max_accel,
         start_velocity=0,
@@ -41,10 +41,10 @@ path_1 = FollowPathCustom(
 path_2 = FollowPathCustom(
     subsystem=Robot.drivetrain,
     trajectory=CustomTrajectory(
-        start_pose=Pose2d(6.45, config.field_width - (1 + 3.66), math.radians(0)),
+        start_pose=Pose2d(6.45, config.field_width - (1 + 3.66 - 0.3), math.radians(0)),
         waypoints=[],
         end_pose=Pose2d(
-            1.8, config.field_width - (0.57 + 4.42 - (1.06 - 0.57)), math.radians(0)
+            1.8, config.field_width - (0.57 + 4.42 - (1.06 - 0.57)) + 0.3, math.radians(0)
         ),
         max_velocity=1.5,
         max_accel=1,
