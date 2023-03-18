52c101314d7e9436a73160353a48496caa01361f
Sidharth Rao <sidharthmrao@gmail.com>
2023-03-18 18:23:45 -0400

    Three piece cable is so close!

--- autonomous/routines/TWO_PIECE_WITH_GUARD/blue.py
+++ autonomous/routines/TWO_PIECE_WITH_GUARD/blue.py
@@ -22,8 +22,8 @@ from command.autonomous.trajectory import CustomTrajectory
 from robot_systems import Robot, Sensors
 from units.SI import meters, meters_per_second, meters_per_second_squared, radians
 
-max_vel: meters_per_second = 3.5
-max_accel: meters_per_second_squared = 2.8
+max_vel: meters_per_second = 4
+max_accel: meters_per_second_squared = 3
 
 initial_x: meters = base_initial_coords[0] + 0
 initial_y: meters = base_initial_coords[1] + 0
@@ -65,6 +65,34 @@ path_2 = FollowPathCustom(
     period=constants.period,
 )
 
+path_3 = FollowPathCustom(
+    subsystem=Robot.drivetrain,
+    trajectory=CustomTrajectory(
+        start_pose=Pose2d(path_2_end_x, path_2_end_y, path_2_end_theta),
+        waypoints=[Translation2d(5.12, 1.22), Translation2d(4.96, 2.3)],
+        end_pose=Pose2d(6.32, 2.3, 0),
+        max_velocity=2.7,
+        max_accel=1.7,
+        start_velocity=0,
+        end_velocity=0,
+    ),
+    period=constants.period,
+)
+
+path_4 = FollowPathCustom(
+    subsystem=Robot.drivetrain,
+    trajectory=CustomTrajectory(
+        start_pose=Pose2d(6.32, 2.3, 0),
+        waypoints=[Translation2d(5.12, 1.22), Translation2d(3.92, 0.92)],
+        end_pose=Pose2d(path_2_end_x, path_2_end_y, path_2_end_theta),
+        max_velocity=3.6,
+        max_accel=2.5,
+        start_velocity=0,
+        end_velocity=0,
+    ),
+    period=constants.period,
+)
+
 auto = SequentialCommandGroup(
     command.ZeroElevator(Robot.arm),
     command.ZeroShoulder(Robot.arm),
@@ -99,7 +127,7 @@ auto = SequentialCommandGroup(
         ],
     ),
     InstantCommand(lambda: Robot.grabber.close_claw()),
-    WaitCommand(0.3),
+    WaitCommand(0.1),
     ParallelDeadlineGroup(
         deadline=path_2,
         commands=[
@@ -112,34 +140,51 @@ auto = SequentialCommandGroup(
             ).generate(),
         ],
     ),
-    command.IntakeDisable(Robot.intake),
-    # ParallelDeadlineGroup(
-    #     deadline=WaitCommand(1),
-    #     commands=[
-    #         command.TargetAuto(
-    #             Robot.arm,
-    #             Robot.grabber,
-    #             Robot.intake,
-    #             Sensors.odometry,
-    #             target=config.scoring_locations["high_auto_back"],
-    #         ).generate()
-    #     ],
-    # ),
+    command.IntakeEnable(Robot.intake),
     InstantCommand(lambda: Robot.grabber.open_claw()),
     InstantCommand(lambda: Robot.grabber.set_output(-0.3)),
-    WaitCommand(0.5),
+    WaitCommand(0.3),
     ParallelDeadlineGroup(
-        deadline=WaitCommand(0.8),
+        deadline=SequentialCommandGroup(path_3, WaitCommand(0)),
         commands=[
-            command.TargetAuto(
-                Robot.arm,
-                Robot.grabber,
-                Robot.intake,
-                Sensors.odometry,
-                target=config.scoring_locations["standard"],
-            ).generate()
+            SequentialCommandGroup(
+                command.TargetAuto(
+                    Robot.arm,
+                    Robot.grabber,
+                    Robot.intake,
+                    Sensors.odometry,
+                    target=config.scoring_locations["cube_intake_auto"],
+                ).generate(),
+            )
         ],
     ),
+    InstantCommand(lambda: Robot.grabber.close_claw()),
+    WaitCommand(0.1),
+    ParallelDeadlineGroup(
+        deadline=path_4,
+        commands=[
+            SequentialCommandGroup(
+                WaitCommand(1),
+                command.TargetAuto(
+                    Robot.arm,
+                    Robot.grabber,
+                    Robot.intake,
+                    Sensors.odometry,
+                    target=config.scoring_locations["mid_auto_back_cube"],
+                ).generate(),
+            )
+        ],
+    ),
+    InstantCommand(lambda: Robot.grabber.open_claw()),
+    InstantCommand(lambda: Robot.grabber.set_output(-0.3)),
+    WaitCommand(0.3),
+    command.TargetAuto(
+        Robot.arm,
+        Robot.grabber,
+        Robot.intake,
+        Sensors.odometry,
+        target=config.scoring_locations["standard"],
+    ).generate(),
 )
 
 routine = AutoRoutine(Pose2d(initial_x, initial_y, initial_theta), auto)
